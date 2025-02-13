b4e400385a96862eb015fe9c3d7b4c504643b8f2
Vitor Samuel Miranda de Souza <Vitorsmiranda31@gmail.com>
2025-02-13 10:23:57 +0100

    cookies

--- src/test/java/com/example/demo/HomeController.java
+++ src/test/java/com/example/demo/HomeController.java
@@ -10,7 +10,6 @@ import org.springframework.ui.Model;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
 
 
 
